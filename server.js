2a49efd95ad48483149ecf194c1c9ca51566d382
Kawaiindah <cecanme@gmail.com>
2023-12-04 00:47:30 +0800

    tambah proccess

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const server = express();
 const cors = require("cors");
 
-server.use(cors())
+server.use(cors());
 
 // nambah router
 const allRouter = require("./routes");
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 3000;
 server.use(express.json());
 server.use(allRouter);
 
-server.listen(env.PORT, () => {
-  console.log("server running on port", env.PORT);
+server.listen(process.env.PORT, () => {
+  console.log('port berjalan di http://localhost:${process.env.PORT}');
 });
 
 //server.get("/api",(request, response) => {
