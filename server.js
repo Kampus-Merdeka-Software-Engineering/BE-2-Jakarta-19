33a87549413f9d9a3275d267591a2ae57a0f24ce
Indah Kurnia <97266958+Kawaiindah@users.noreply.github.com>
2023-12-04 10:39:14 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ server.use(cors());
 // nambah router
 const allRouter = require("./routes");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 server.use(express.json());
