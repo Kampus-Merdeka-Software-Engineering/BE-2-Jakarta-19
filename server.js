2194763bec89ded3b228417886087e64907cd140
Kawaiindah <cecanme@gmail.com>
2023-12-11 13:33:54 +0800

    done

--- server.js
+++ server.js
@@ -7,18 +7,14 @@ server.use(cors());
 
 const allRouter = require("./routes");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(allRouter);
 
-<<<<<<< HEAD
 server.listen(PORT,"0.0.0.0",  () => {
-=======
-server.listen(PORT, "0.0.0.0", () => {
->>>>>>> a3f72a509de33111b3cc08af6005555cb7939be6
     console.log(`port berjalan di http://localhost:${PORT}`);
 });
 
