7404fe7ead456e05aeadf003e9462fb322c14eeb
Kawaiindah <cecanme@gmail.com>
2023-12-11 13:22:18 +0800

    secret

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 server.use(cors());
 
-// nambah router
+
 const allRouter = require("./routes");
 
 const PORT = process.env.PORT || 3000;
@@ -14,14 +14,7 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(allRouter);
 
-server.listen(PORT,"0.0.0.0", () => {
+server.listen(PORT,"0.0.0.0",  () => {
     console.log(`port berjalan di http://localhost:${PORT}`);
 });
-
-//server.get("/api",(request, response) => {
-   // response.status(200).json({
-       // data: {},
-       // message: "Succesfully retrieved inda"
-    //});
-//});
 
